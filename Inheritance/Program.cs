791c84462c78662381212cd89410695fffb5cafa
Jason Moore <moorejbm@gmail.com>
2021-02-03 14:02:09 -0600

    Update3

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,15 +8,15 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // DONE Create a class Animal
             // give this class 4 members that all Animals have in common
 
 
-            // Create a class Bird
+            // DONE Create a class Bird
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
-            // Create a class Reptile
+            // DONE Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
@@ -29,11 +29,39 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
+            Bird myBird = new Bird();
+            myBird.WingColor = "black";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 2.7;
+
+            ///*Create an object of your Reptile class
+            //*  give values to your members using the object of your Bird class
+            //* Creatively display the class member values 
+
+            var lizard = new Reptiles()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true,
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach (var anamal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{anamal.IsAlive}");
+                Console.WriteLine($"Age: {anamal.Age} years old");
+                Console.WriteLine($"It Has {anamal.LegCount} legs");
+                Console.WriteLine($"It lives by {anamal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+
         }
-    }
+
+
+    }        
+             
 }
+
---
+++ Inheritance/Reptiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Reptiles : Animal
+    {
+        public Reptiles()
+        {
+            IsAlive = true;
+            LegCount = 4;
+            LandSeaAir = "Land";
+            Age = 0;
+        }
+
+        public bool IsColdBlooded { get; set; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+    }
+}
